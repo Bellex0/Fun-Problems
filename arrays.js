9297d770d94690fd6d2b283f3a830a0666a712c3
bellex0 <bellepx3@gmail.com>
2020-02-18 04:24:15 -0500

    problem 2

--- arrays.js
+++ arrays.js
@@ -16,3 +16,33 @@ function fizzBuzz(n){
 
 
 fizzBuzz(5)
+
+let myMatrix = [[1,2,3,4],
+                [5,6,7,8],
+                [9,10,11,12],
+                [13,14,15,16]];
+
+
+function unroll(matrix) {
+  
+	if (matrix.length === 0) return;
+	
+	// top
+	console.log(...matrix.shift());
+	
+	// right (last elem of each)
+	console.log(...matrix.map(arr => arr.pop()))
+	
+	// bottom
+	console.log(...matrix.pop().reverse());
+
+	// left (first elem of each)
+	console.log(...matrix.map(arr => arr.shift()).reverse());
+
+	unroll(matrix);
+
+}
+
+unroll(myMatrix);
+
+// end result: 1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10
