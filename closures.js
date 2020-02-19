675a3d9fc40c28448636a3c4196569c23102f8a5
bellex0 <bellepx3@gmail.com>
2020-02-19 00:16:42 -0500

    closures

---
+++ closures.js
@@ -0,0 +1,23 @@
+function getSecret(x){
+const b = x
+const a = 2
+return ( () => a+b )
+}
+
+
+
+
+// Getsecret(3)() = 5 
+getSecret(3)
+
+
+const num = 3
+
+function hello(x){
+let b = num + 5  
+return x * b 
+}
+
+hello(2)
+
+X * 5 ← pure 
