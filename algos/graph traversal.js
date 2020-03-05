a1612a96fdf1f7277184bb0c7db54a2d12245748
bellex0 <bellepx3@gmail.com>
2020-03-04 21:16:53 -0500

    remove edge

--- algos/graph traversal.js
+++ algos/graph traversal.js
@@ -13,6 +13,34 @@ class Graph{
 }
 
 let g = new Graph();
-g.addVertex("Dallas");
-g.addVertex("Tokyo");
-g.addVertex("Aspen");
+g.addVertex("My house");
+g.addVertex("Disney World");
+g.addVertex("Arches National Park");
+
+class Graph{
+    constructor(){
+        this.adjacencyList = {};
+    }
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+    addEdge(v1,v2){
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
+    removeEdge(vertex1,vertex2){
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+            v => v !== vertex2
+        );
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+            v => v !== vertex1
+        );
+    }
+}
+
+let g = new Graph();
+g.addVertex("My house");
+g.addVertex("Disney World");
+g.addVertex("Arches National Park");
+g.addEdge("My house", "Disney World");
+g.addEdge("My House", "Arches National Park");
